6612243bbef39b52fd6a6f1b37b7173c503461d3
Roger <roger-santos74@hotmail.com>
2020-03-13 19:37:08 -0300

    first commit

---
+++ outroteste.py
@@ -0,0 +1,63 @@
+import imaplib,email
+import time
+import datetime as dt 
+from os import path
+from datetime import timezone
+
+user = "abc@gmail.com"
+password = "password"
+imap_url = "imap.gmail.com"
+
+def get_body(msg):
+    if msg.is_multipart():
+        return get_body(msg.get_payload(0))
+    else:
+        return msg.get_payload(None,True)
+date_time = dt.datetime.now()
+
+f = open("gmail_time.txt","r") 
+
+a = f.read()
+f.close()
+
+a = dt.datetime.strptime(a, '%a, %d %b %Y %H:%M:%S %z ')
+
+date_time.replace(tzinfo=timezone.utc)
+
+date_time = date_time.replace(tzinfo=timezone.utc).isoformat()
+
+date_time = dt.datetime.strptime(date_time, '%Y-%m-%dT%H:%M:%S.%f%z')
+
+lastDayDateTime = date_time - a
+
+con = imaplib.IMAP4_SSL(imap_url)
+con.login(user,password)
+con.select('INBOX')
+
+dates= (dt.datetime.now() - lastDayDateTime).strptime("%d-%b-%Y")
+resp = con.uid('search', None, ('SENTSINCE {date})'.format(date=dates)) 
+items = con.uid('search', None, ('SENTSINCE {date})'.format(date=dates))
+
+a = items
+b = a [0].decode('utf-8')
+f1=open("gmail_mails.txt","w+")
+
+for .value in b:
+     late= int(value)-801
+     late= srt(late)
+     result,data= con.fetch(late,'(RFC822)')
+     raw= email.message_from_bytes(data[0][1])
+     print (raw['Date'])
+     print (raw['From'])
+     print (raw['Body'])
+     f1.write('Date = '+raw['Date'] + ' ' )
+     f1.write('From = '+raw['From'] + ' ' )
+     f1.write('Subject = '+raw['Subject'] + ' ' )
+     f1.write('Body = '+str(get_body(raw)) + ' ' )
+     f1.write(' ')
+f1.close()
+f2 =open("gmail_time.txt","w+")
+f2.write(raw['Date'])
+f2.close
+
+
---
+++ quickstart.py
@@ -0,0 +1,83 @@
+from __future__ import print_function
+import pickle
+import os.path
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+
+import base64
+from email.mime.text import MIMEText
+
+
+# If modifying these scopes, delete the file token.pickle.
+SCOPES = ['https://www.googleapis.com/auth/gmail.readonly']
+
+def main():
+    """Shows basic usage of the Gmail API.
+    Lists the user's Gmail labels.
+    """
+    creds = None
+    # The file token.pickle stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists('token.pickle'):
+        with open('token.pickle', 'rb') as token:
+            creds = pickle.load(token)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'credentials.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open('token.pickle', 'wb') as token:
+            pickle.dump(creds, token)
+
+    service = build('gmail', 'v1', credentials=creds)
+
+    # Call the Gmail
+    results = service.users().labels().list(userId='me').execute()
+    labels = results.get('labels', [])
+
+    if not labels:
+        print('No labels found.')
+    else:
+        print('Labels:')
+        for label in labels:
+            print(label['name'])
+    
+    MSG = create_message('teste', 'tony.carvalho3037@gmail.com', 'e-mail automatico', 'deu certo?')
+    send_message(service, 'me', MSG)
+
+    print("FIM")
+
+
+
+
+"""Create a message for an email.
+
+  Args:
+    sender: Email address of the sender.
+    to: Email address of the receiver.
+    subject: The subject of the email message.
+    message_text: The text of the email message.
+
+  Returns:
+    An object containing a base64url encoded email object.
+"""
+def create_message(sender, to, subject, message_text):
+  message = MIMEText(message_text)
+  message['to'] = to
+  message['from'] = sender
+  message['subject'] = subject
+  return {'raw': base64.urlsafe_b64encode(message.as_bytes())}
+
+def send_message(service, user_id, message):
+    message = (service.users().messages().send(userId=user_id, body=message).execute())
+    print ('Message Id: %s' % message['id'])
+    return message
+
+if __name__ == '__main__':
+    main()
